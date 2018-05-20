2308bdf664658b6887a39f04fc39c48d407cf26a
parijatmalla <parijat.malla@gmail.com>
2018-05-20 14:19:55 -0400

    Adding files for Project 1

--- Course5/Week1/Project1.R
+++ Course5/Week1/Project1.R
@@ -16,8 +16,8 @@ readData<-function (){
 		
 	}
 	file<-"./activity.csv"
-	activity<-read.csv(file,header=TRUE, sep=",")
-return (activity)
+#	activity<-read.csv(file,header=TRUE, sep=",")
+#return (activity)
 }
 
 plotHist<-function()
@@ -25,8 +25,8 @@ plotHist<-function()
 	readData()
 	
 	file<-"./activity.csv"
-	activity<-read.csv(file,header=TRUE, sep=",")
-	activity<-activity[with(activity,{!(is.na(steps))}),]
+	activityNA<-read.csv(file,header=TRUE, sep=",",stringsAsFactors=FALSE)
+	activity<-activityNA[with(activityNA,{!(is.na(steps))}),]
 	#	data_row[ with (data_row, { !(is.na(steps)) } ), ]
 	
 	activity$date<-as.Date(activity$date,format="%Y-%m-%d")
@@ -42,4 +42,34 @@ plotHist<-function()
 
 	hist(s$steps,main="Histogram of Total Steps per Day",xlab="Steps Per Day")
 
+	groupedData<-group_by(activity,interval)
+	mean_steps<-summarize(groupedData, steps=mean(steps))
+	
+	activityOnlyNA<-activityNA[with(activityNA,{(is.na(steps))}),]
+	activityOnlyNA<-select(activityOnlyNA,interval,date)
+	imputedData<-merge(activityOnlyNA,mean_steps,by.x="interval", by.y="interval")
+	imputedData<-data.frame(select(imputedData, steps,date,interval),stringsAsFactors = FALSE)
+	activity<-data.frame(select(activity,steps,date,interval),stringsAsFactors = FALSE)
+	imputedData$date<-as.Date(imputedData$date,format="%Y-%m-%d")
+	activityImputed<-rbind(imputedData,activity)
+	c<-class(activityImputed$date)
+	activityImputed$date<-as.Date(activityImputed$date,format="%Y-%m-%d")
+	
+x<-mutate(activityImputed,Dayofweek=weekdays(as.Date(date)))
+a<- filter(activityImputed,weekdays(as.Date(date)) %in% c("Saturday","Sunday") )
+#	nrow(a)
+	a<-mutate(a,Dayofweek ="Weekend") 
+	
+	#aa<- filter(activity,weekdays(as.Date(date)) %in% c("Saturday","Sunday") )
+#	nrow(aa)
+#	aa<-mutate(aa,Dayofweek ="Weekend") 
+	
+	b<- filter(activityImputed,!(weekdays(date) %in% c("Saturday","Sunday") ))
+b<-mutate(b,Dayofweek ="Weekday") 
+	
+	
+	
+	z<-nrow(a)
+nrow(b)
+	return(activityImputed)
 }
